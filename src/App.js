bb068e6dbee770de364b9073d1017bc5945847bf
alexis-tao <tao.alexis@gmail.com>
2020-02-20 01:31:03 -0500

    change path

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 
-function App() {
-  return (
-    <Router>
-      <Route path="/" exact component={withRouter(Login)} />
-      <Route path="/home" exact component={withRouter(Home)} />
-    </Router>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route
+          path={process.env.PUBLIC_URL}
+          exact
+          component={withRouter(Login)}
+        />
+        <Route path={'/home'} exact component={withRouter(Home)} />
+      </Router>
+    );
+  }
 }
-
-export default App;
