74eecb9f533170416abb98a20cf25d217cfc4874
alexis-tao <tao.alexis@gmail.com>
2020-02-20 01:34:49 -0500

    change router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import { HashRouter, Route, withRouter } from 'react-router-dom';
 import Login from './Login';
 import Home from './Home';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
+      <HashRouter basename="/">
         <Route
           path={process.env.PUBLIC_URL}
           exact
           component={withRouter(Login)}
         />
         <Route path={'/home'} exact component={withRouter(Home)} />
-      </Router>
+      </HashRouter>
     );
   }
 }
